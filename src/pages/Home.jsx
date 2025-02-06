6e3073a58380dcfb546c2d08d676d8052f966344
Damian Matuszak <damian_matuszak@o2.pl>
2025-02-06 19:35:12 +0100

    add dynamic fetching

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,51 @@
+import { useEffect, useState } from "react";
 import { Layout } from "../components/Layout/Layout";
 import { Postview } from "../components/Postrview";
 
-export const Home = () => (
+// const posts = [
+//     {
+//          user: "Zbigniew",
+//          time:"12 minutes ago", 
+//          avatar:"avatar.jpg", 
+//          postimage:"post.png",
+//          description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus"
+//         },
+//     {user: "Adam",
+//         time:"15 minutes ago", 
+//         avatar:"user.png", 
+//         postimage:"post.png",
+//         description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus"},
+//     {user: "Ewa",
+//         time:"12:30", 
+//         avatar:"avatar.jpg", 
+//         postimage:"post.png",
+//         description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus"},
+//     {user: "Józek",
+//         time:"12:30", 
+//         avatar:"user.png", 
+//         postimage:"post.png",
+//         description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla dolor, ornare at commodo non, feugiat non nisi. Phasellus faucibus mollis pharetra. Proin blandit ac massa sed rhoncus"}
+// ];
+
+
+export const Home = () => {
+const [posts, setPosts] = useState();
+//HOOK
+useEffect(() => {
+    fetch("https://my.api.mockaroo.com/posts.json?key=6d1737e0")
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
+  }, []); //pobieranie danych z backendu
+
+  console.log('posts from Home component: ', posts);
+  const updatedPosts = posts?.map((post) => {
+    return {
+      ...post,
+      postimage: `https://picsum.photos/id/${post.id}/800/400`,
+    };
+  });
+
+return (
     <Layout>
 
         <div className="main-content right-chat-active">
@@ -9,14 +53,13 @@ export const Home = () => (
                 <div className="middle-sidebar-left">
                     <div className="row feed-body">
                         <div className="col-xl-8 col-xxl-9 col-lg-8">
-                            <Postview user="Zbigniew" time="12:30" avatar="avatar.jpg"/>
-                            <Postview user="Adam" time="12:00" avatar="user.png"/> 
-                            <Postview user="Ewa" time="12:35" avatar="avatar.jpg"/>
-                            <Postview user="Józek" time="12:33" avatar="user.png"/>
+                            {updatedPosts && updatedPosts.length > 0 && updatedPosts.map((post) => (
+                                <Postview key={post.id} {...post} /> // uproszczony zapisa Mapowania +destrukturyzacja
+                            ))}
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </Layout>
-);
+)};
